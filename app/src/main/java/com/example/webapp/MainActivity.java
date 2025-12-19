9dd301c130df11cdb97dc4804a03b5edb1a117e0
wumatteo672-dev <wumatteo672@gmail.com>
2025-12-19 18:55:05 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/webapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.blueo.admin;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.beduelsminter.cloud/1026/login.php");
+    }
+}
